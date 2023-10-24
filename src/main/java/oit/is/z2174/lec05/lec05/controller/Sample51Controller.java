ea1b0ef41029bdd68eb9bbf4bf666c38bef79c5e
Shibata1313 <e1b21047@oit.ac.jp>
2023-10-24 15:07:26 +0900

    果物データを編集する

--- src/main/java/oit/is/z2174/lec05/lec05/controller/Sample51Controller.java
+++ src/main/java/oit/is/z2174/lec05/lec05/controller/Sample51Controller.java
@@ -73,22 +73,22 @@ public class Sample51Controller {
   // * @param model
   // * @return
   // */
-  // @PostMapping("step5")
-  // public String sample55(@RequestParam Integer id, @RequestParam String name,
-  // @RequestParam Integer price,
-  // ModelMap model) {
-  // System.out.println("step5");
-  // System.out.println(id);
-  // System.out.println(name);
-  // System.out.println(price);
-  // Fruit fruit = new Fruit(id, name, price);
-  // // update
-  // fMapper.updateById(fruit);
-  // // フルーツリストを取得
-  // ArrayList<Fruit> fruits2 = fMapper.selectAllFruit();
-  // model.addAttribute("fruits2", fruits2);
+  @PostMapping("step5")
+  public String sample55(@RequestParam Integer id, @RequestParam String name,
+      @RequestParam Integer price,
+      ModelMap model) {
+    System.out.println("step5");
+    System.out.println(id);
+    System.out.println(name);
+    System.out.println(price);
+    Fruit fruit = new Fruit(id, name, price);
+    // update
+    fMapper.updateById(fruit);
+    // フルーツリストを取得
+    ArrayList<Fruit> fruits2 = fMapper.selectAllFruit();
+    model.addAttribute("fruits2", fruits2);
 
-  // return "sample51.html";
-  // }
+    return "sample51.html";
+  }
 
 }
